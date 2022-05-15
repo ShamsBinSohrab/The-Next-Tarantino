4f77a8eaa506ce943b1853808a7fb8227f2c3506
ShamsBinSohrab <aniknishan@gmail.com>
2022-05-15 12:03:46 +0600

    Fixed userId in TokenProvider

--- src/main/java/com/shams/tarantino/security/jwt/TokenProvider.java
+++ src/main/java/com/shams/tarantino/security/jwt/TokenProvider.java
@@ -31,123 +31,126 @@ import tech.jhipster.config.JHipsterProperties;
 @Component
 public class TokenProvider {
 
-  private final Logger log = LoggerFactory.getLogger(TokenProvider.class);
+    private final Logger log = LoggerFactory.getLogger(TokenProvider.class);
 
-  private static final String AUTHORITIES_KEY = "auth";
-  private static final String USER_ID_KEY = "auth";
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String USER_ID_KEY = "auth";
 
-  private static final String INVALID_JWT_TOKEN = "Invalid JWT token.";
+    private static final String INVALID_JWT_TOKEN = "Invalid JWT token.";
 
-  private final Key key;
+    private final Key key;
 
-  private final JwtParser jwtParser;
+    private final JwtParser jwtParser;
 
-  private final long tokenValidityInMilliseconds;
+    private final long tokenValidityInMilliseconds;
 
-  private final long tokenValidityInMillisecondsForRememberMe;
+    private final long tokenValidityInMillisecondsForRememberMe;
 
-  private final SecurityMetersService securityMetersService;
+    private final SecurityMetersService securityMetersService;
 
-  public TokenProvider(
-      JHipsterProperties jHipsterProperties, SecurityMetersService securityMetersService) {
-    byte[] keyBytes;
-    String secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getBase64Secret();
-    if (!ObjectUtils.isEmpty(secret)) {
-      log.debug("Using a Base64-encoded JWT secret key");
-      keyBytes = Decoders.BASE64.decode(secret);
-    } else {
-      log.warn(
-          "Warning: the JWT key used is not Base64-encoded. "
-              + "We recommend using the `jhipster.security.authentication.jwt.base64-secret` key for optimum security.");
-      secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
-      keyBytes = secret.getBytes(StandardCharsets.UTF_8);
-    }
-    key = Keys.hmacShaKeyFor(keyBytes);
-    jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
-    this.tokenValidityInMilliseconds =
-        1000
-            * jHipsterProperties
+    public TokenProvider(
+        JHipsterProperties jHipsterProperties, SecurityMetersService securityMetersService) {
+        byte[] keyBytes;
+        String secret = jHipsterProperties.getSecurity().getAuthentication().getJwt()
+            .getBase64Secret();
+        if (!ObjectUtils.isEmpty(secret)) {
+            log.debug("Using a Base64-encoded JWT secret key");
+            keyBytes = Decoders.BASE64.decode(secret);
+        } else {
+            log.warn(
+                "Warning: the JWT key used is not Base64-encoded. "
+                    + "We recommend using the `jhipster.security.authentication.jwt.base64-secret` key for optimum security.");
+            secret = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
+            keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        }
+        key = Keys.hmacShaKeyFor(keyBytes);
+        jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+        this.tokenValidityInMilliseconds =
+            1000
+                * jHipsterProperties
                 .getSecurity()
                 .getAuthentication()
                 .getJwt()
                 .getTokenValidityInSeconds();
-    this.tokenValidityInMillisecondsForRememberMe =
-        1000
-            * jHipsterProperties
+        this.tokenValidityInMillisecondsForRememberMe =
+            1000
+                * jHipsterProperties
                 .getSecurity()
                 .getAuthentication()
                 .getJwt()
                 .getTokenValidityInSecondsForRememberMe();
 
-    this.securityMetersService = securityMetersService;
-  }
-
-  public String createToken(Authentication authentication, boolean rememberMe) {
-    var user = (UserDetailsImpl) authentication.getPrincipal();
-    String authorities =
-        user.getAuthorities().stream()
-            .map(GrantedAuthority::getAuthority)
-            .collect(Collectors.joining(","));
-
-    long now = (new Date()).getTime();
-    Date validity;
-    if (rememberMe) {
-      validity = new Date(now + this.tokenValidityInMillisecondsForRememberMe);
-    } else {
-      validity = new Date(now + this.tokenValidityInMilliseconds);
+        this.securityMetersService = securityMetersService;
     }
 
-    return Jwts.builder()
-        .setSubject(user.getUsername())
-        .claim(AUTHORITIES_KEY, authorities)
-        .claim(USER_ID_KEY, user.getId())
-        .signWith(key, SignatureAlgorithm.HS512)
-        .setExpiration(validity)
-        .compact();
-  }
+    public String createToken(Authentication authentication, boolean rememberMe) {
+        var user = (UserDetailsImpl) authentication.getPrincipal();
+        String authorities =
+            user.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
 
-  public Authentication getAuthentication(String token) {
-    Claims claims = jwtParser.parseClaimsJws(token).getBody();
+        long now = (new Date()).getTime();
+        Date validity;
+        if (rememberMe) {
+            validity = new Date(now + this.tokenValidityInMillisecondsForRememberMe);
+        } else {
+            validity = new Date(now + this.tokenValidityInMilliseconds);
+        }
 
-    Collection<? extends GrantedAuthority> authorities =
-        Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-            .filter(auth -> !auth.trim().isEmpty())
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toList());
-
-    var principal = new UserDetailsImpl(1L, claims.getSubject(), "", authorities);
-
-    return new UsernamePasswordAuthenticationToken(principal, token, authorities);
-  }
-
-  public boolean validateToken(String authToken) {
-    try {
-      jwtParser.parseClaimsJws(authToken);
-
-      return true;
-    } catch (ExpiredJwtException e) {
-      this.securityMetersService.trackTokenExpired();
-
-      log.trace(INVALID_JWT_TOKEN, e);
-    } catch (UnsupportedJwtException e) {
-      this.securityMetersService.trackTokenUnsupported();
-
-      log.trace(INVALID_JWT_TOKEN, e);
-    } catch (MalformedJwtException e) {
-      this.securityMetersService.trackTokenMalformed();
-
-      log.trace(INVALID_JWT_TOKEN, e);
-    } catch (SignatureException e) {
-      this.securityMetersService.trackTokenInvalidSignature();
-
-      log.trace(INVALID_JWT_TOKEN, e);
-    } catch (
-        IllegalArgumentException
-            e) { // TODO: should we let it bubble (no catch), to avoid defensive programming and
-      // follow the fail-fast principle?
-      log.error("Token validation error {}", e.getMessage());
+        return Jwts.builder()
+            .setSubject(user.getUsername())
+            .claim(AUTHORITIES_KEY, authorities)
+            .claim(USER_ID_KEY, user.getId())
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setExpiration(validity)
+            .compact();
     }
 
-    return false;
-  }
+    public Authentication getAuthentication(String token) {
+        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+            Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .filter(auth -> !auth.trim().isEmpty())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        var userId = (Integer) claims.get(USER_ID_KEY);
+        var principal = new UserDetailsImpl(userId.longValue(), claims.getSubject(), "",
+            authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            jwtParser.parseClaimsJws(authToken);
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            this.securityMetersService.trackTokenExpired();
+
+            log.trace(INVALID_JWT_TOKEN, e);
+        } catch (UnsupportedJwtException e) {
+            this.securityMetersService.trackTokenUnsupported();
+
+            log.trace(INVALID_JWT_TOKEN, e);
+        } catch (MalformedJwtException e) {
+            this.securityMetersService.trackTokenMalformed();
+
+            log.trace(INVALID_JWT_TOKEN, e);
+        } catch (SignatureException e) {
+            this.securityMetersService.trackTokenInvalidSignature();
+
+            log.trace(INVALID_JWT_TOKEN, e);
+        } catch (
+            IllegalArgumentException
+                e) { // TODO: should we let it bubble (no catch), to avoid defensive programming and
+            // follow the fail-fast principle?
+            log.error("Token validation error {}", e.getMessage());
+        }
+
+        return false;
+    }
 }
