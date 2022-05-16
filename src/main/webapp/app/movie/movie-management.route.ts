a081c57d067d60c9633f732d9e3306b721179ce6
ShamsBinSohrab <aniknishan@gmail.com>
2022-05-16 15:29:57 +0600

    fixed route issues

--- src/main/webapp/app/movie/movie-management.route.ts
+++ src/main/webapp/app/movie/movie-management.route.ts
@@ -7,46 +7,50 @@ import {MovieService} from "./service/movie.service";
 import {IMovieDetails, IMovieReview} from "./movie-management.model";
 import {MovieManagementDetailsComponent} from "./details/movie-management-details.component";
 import {MovieManagementReviewComponent} from "./review/movie-management-review.component";
+import {UserRouteAccessService} from "../core/auth/user-route-access.service";
 
 @Injectable({providedIn: 'root'})
 export class MovieDetailsResolve implements Resolve<IMovieDetails> {
-    constructor(private service: MovieService) {
-    }
+  constructor(private service: MovieService) {
+  }
 
-    resolve(route: ActivatedRouteSnapshot): Observable<IMovieDetails> {
-        const id = route.params['imdbId'];
-        return this.service.details(id);
-    }
+  resolve(route: ActivatedRouteSnapshot): Observable<IMovieDetails> {
+    const id = route.params['imdbId'];
+    return this.service.details(id);
+  }
 }
 
 @Injectable({providedIn: 'root'})
 export class MovieReviewResolve implements Resolve<IMovieReview> {
-    constructor(private service: MovieService) {
-    }
+  constructor(private service: MovieService) {
+  }
 
-    resolve(route: ActivatedRouteSnapshot): Observable<IMovieReview> {
-        const id = route.params['id'];
-        return this.service.getReview(id);
-    }
+  resolve(route: ActivatedRouteSnapshot): Observable<IMovieReview> {
+    const id = route.params['id'];
+    return this.service.getReview(id);
+  }
 }
 
 export const movieRoute: Routes = [
-    {
-        path: '',
-        component: MovieManagementComponent,
+  {
+    path: '',
+    component: MovieManagementComponent,
+    canActivate: [UserRouteAccessService]
+  },
+  {
+    path: ':imdbId/view',
+    component: MovieManagementDetailsComponent,
+    resolve: {
+      details: MovieDetailsResolve,
     },
-    {
-        path: ':imdbId/view',
-        component: MovieManagementDetailsComponent,
-        resolve: {
-            details: MovieDetailsResolve,
-        }
+    canActivate: [UserRouteAccessService]
+  },
+  {
+    path: ':id/review',
+    component: MovieManagementReviewComponent,
+    resolve: {
+      review: MovieReviewResolve,
     },
-    {
-        path: ':id/review',
-        component: MovieManagementReviewComponent,
-        resolve: {
-            review: MovieReviewResolve,
-        }
-    },
+    canActivate: [UserRouteAccessService]
+  },
 ];
